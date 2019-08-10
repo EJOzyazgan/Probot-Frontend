65381463ee6f20c9e0f6c77c6cabfe97af65d481
EJ Ozyazgan <ejozyazgan@gmail.com>
2019-08-10 12:19:37 -0700

    Probot-68 support

---
+++ src/app/platform/support/support.component.ts
@@ -0,0 +1,55 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from '../../models/user.model';
+import {AuthService} from '../../services/auth.service';
+import {AlertService} from 'ngx-alerts';
+
+@Component({
+  selector: 'app-support',
+  templateUrl: './support.component.html',
+  styleUrls: ['./support.component.scss']
+})
+export class SupportComponent implements OnInit {
+  user = new User();
+
+  name;
+  email;
+  message;
+
+  constructor(private authService: AuthService,
+              private alertService: AlertService) {
+  }
+
+  ngOnInit() {
+    this.getUser();
+  }
+
+  getUser() {
+    this.authService.getUser().subscribe(user => {
+      this.user = user;
+      if (this.user) {
+        this.email = this.user.email;
+        this.name = this.user.username;
+      }
+    });
+  }
+
+  validForm() {
+    return this.name && this.name.trim() !== '' &&
+      this.email && this.email.trim() !== '' &&
+      this.message && this.message.trim() !== '';
+  }
+
+  submit() {
+    const body = {
+      name: this.name,
+      email: this.email,
+      message: this.message,
+    };
+
+    this.authService.sendSupport(body).subscribe(email => {
+      this.alertService.success(email['msg']);
+    }, error => {
+      this.alertService.danger(error['error']['error']['msg']);
+    });
+  }
+}
