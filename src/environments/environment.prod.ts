cbf84d192b893d37170cf99c7231e47f763b1303
EJ Ozyazgan <ejozyazgan@gmail.com>
2019-08-31 02:34:58 -0700

    Prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/',
+  apiUrl: 'probot-playground.us-west-2.elasticbeanstalk.com/',
   userTokenKey: 'probot-user-token',
   userTokenExpire: 'probot-user-expire',
   userRefreshToken: 'probot-user-refresh',
